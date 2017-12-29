0b2e1507910cca598211cdc7fb1bd5f131561a3a
heybluguy <heybluguy@yahoo.com>
2017-12-28 20:28:39 -0500

    added updating jobs functionality

--- spec/features/jobs/user_can_update_job_spec.rb
+++ spec/features/jobs/user_can_update_job_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
-# describe "as a user" do
-#   it "i can update a job" do
-#     company = Company.create!(name: "ESPN")
-#     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+describe "as a user" do
+  it "I can update a job" do
+    company = Company.create!(name: "ESPN")
+    category = Category.create!(title: "Engineering")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
 
-#     visit edit_company_job_path(company, job)
-#     fill_in "job[title]", with: "Developer"
-#     fill_in "job[description]", with: "So fun!"
-#     fill_in "job[level_of_interest]", with: 80
-#     fill_in "job[city]", with: "New York City"
+    visit edit_company_job_path(company, job)
 
-#     click_button "Update"
+    fill_in "job[title]", with: "Developer"
+    fill_in "job[description]", with: "So fun!"
+    fill_in "job[level_of_interest]", with: 80
+    fill_in "job[city]", with: "New York City"
+    select("Engineering", :from => "job[category_id]")
 
-#     expect(page).to have_content("New York City")
-#   end
+    click_button "Update Job"
+    expect(page).to have_content("New York City")
+  end
 
-# end
+end
