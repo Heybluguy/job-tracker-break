8b610d8edc5e94f2f2c55d5e3e32958c9ae7b840
heybluguy <heybluguy@yahoo.com>
2017-12-30 20:46:14 -0500

    Finished functionality for comments

--- spec/features/comments/user_can_add_multiple_comments_to_job_spec.rb
+++ spec/features/comments/user_can_add_multiple_comments_to_job_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 describe "as a user" do
-  scenario "i can can add a comment to a job" do
+  scenario "i can can add multiple comments to a job" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
     visit company_job_path(company, job)
 
     fill_in "comment[content]",	with: "Spoke to hiring manager, Jacob. Plan to follow up Monday."
     click_on "Post Comment"
-
+    fill_in "comment[content]",	with: "Hiring manager wants to do coffee."
+    click_on "Post Comment"
+save_and_open_page
     expect(current_path).to eq(company_job_path(company, job))
     expect(page).to have_content("Spoke to hiring manager, Jacob. Plan to follow up Monday.")
   end
 end
+
+# The user can leave multiple comments on a job 
+# and the most recent comments are shown above older comments (in reverse of the order in which they were created).
